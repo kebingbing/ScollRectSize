0a5469faac04b4eab1b71b2089e2eda0838191e4
kebingbing <378607905@qq.com>
2017-11-07 18:25:06 +0800

    Add files via upload

---
+++ ItemSize.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSize : MonoBehaviour {
+
+    public ScrollRectSize ss;
+    UnityEngine.UI.GridLayoutGroup glg;
+    RectTransform rt;
+    RectTransform node;
+    // Use this for initialization
+    void Start () {
+        ss = GetComponentInParent<ScrollRectSize>();
+        if(ss != null)
+        {
+            ss.AddItem(this);
+        }
+
+        node = transform.Find("Image").GetComponent<RectTransform>();
+        rt = transform.GetComponent<RectTransform>();
+        glg = ss.content.GetComponent<UnityEngine.UI.GridLayoutGroup>();
+	}
+
+    public float ItemX
+    {
+        get
+        {
+            return 2*(glg.cellSize.x + glg.spacing.x);
+        }
+    }
+
+    public void NotifyChange()
+    {
+        float delt = Mathf.Abs(ss.content.anchoredPosition.x + rt.anchoredPosition.x - ItemX);
+        float scale = 1 - delt / ItemX;
+
+        if (scale < -0.5f)
+        {
+            scale = 0.6f;
+        }
+        else if(scale > 0.95f)
+        {
+
+            scale = 1;
+        }
+        else
+        {
+            scale = Mathf.Max(scale, 0.6f);
+        }
+        
+        node.localScale = new Vector3(1.1f,1.1f,1)*scale;
+
+    }
+
+    public Vector2 ContentCenterPos()
+    {
+        Vector2 pos = new Vector2(0,0);
+
+        return node.sizeDelta;
+    }
+
+
+}
---
+++ ScrollRectSize.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ScrollRectSize : UnityEngine.UI.ScrollRect
+{
+
+
+    public List<ItemSize> ItemSizeList = new List<ItemSize>();
+    public void AddItem(ItemSize item)
+    {
+        if(!ItemSizeList.Contains(item))
+        {
+            ItemSizeList.Add(item);
+        }
+    }
+
+    public void NotifyContentMoveIng()
+    {
+        var iter = ItemSizeList.GetEnumerator();
+        while(iter.MoveNext())
+        {
+            iter.Current.NotifyChange();
+        }
+    }
+
+    protected override void SetContentAnchoredPosition(Vector2 position)
+    {
+        base.SetContentAnchoredPosition(position);
+        NotifyContentMoveIng();
+    }
+
+    public override void LayoutComplete()
+    {
+        base.LayoutComplete();
+        NotifyContentMoveIng();
+    }
+    //public override void OnBeginDrag(PointerEventData eventData)
+    //{
+    //    base.OnBeginDrag(eventData);
+    //}
+
+    //public override void OnDrag(PointerEventData eventData)
+    //{
+    //    base.OnDrag(eventData);
+    //    NotifyContentMoveIng();
+    //    //Debug.Log(eventData.ToString());
+    //}
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+
+    }
+
+
+
+}
+
